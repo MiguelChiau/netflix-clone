326b21fbbc49b2714a7e615f3aa772aa313a84b0
Miguel Chiau <chiau.miguel@gmail.com>
2020-11-13 14:00:13 -0800

    table complete

--- src/Table.js
+++ src/Table.js
@@ -8,6 +8,9 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import CloseIcon from '@material-ui/icons/Close';
+import CheckIcon from '@material-ui/icons/Check';
+
 const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: theme.palette.common.black,
@@ -30,11 +33,14 @@ function createData(name, BASIC, STANDARD, PREMIUM) {
   return { name, BASIC, STANDARD, PREMIUM};
 }
 
+const icon1 = CloseIcon;
+
 const rows = [
+
   createData('Monthly price after free month ends on 6/19/19', `$8.99`, `$12.99`, `$15.99`),
-  createData('HD Available', `$9`, `$13`, `$16`),
+  createData('HD Available', ``, `$13`, `$16`),
   createData('Ultra HD Available', 262, 16.0, 24),
-  createData('Screens you can watch on at the same time', 305, 3.7, 67),
+  createData('Screens you can watch on at the same time', 1, 2, 4),
   createData('Watch on your laptop, TV, phone and tablet', 356, 16.0, 49),
   createData('Unlimited movies and TV shows', 356, 16.0, 49),
   createData('Cancel anytime', 356, 16.0, 49),
@@ -43,18 +49,24 @@ const rows = [
 
 ];
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
-});
+// const useStyles = makeStyles({
+//   table: {
+//     maxWidth: 750,
+//     // width: 100%;
+	
+//   },
+// });
 
 export default function CustomizedTables() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
+    <div style={{width:"750px", marginLeft: "auto",
+  marginRight: "auto", marginTop: "2rem"}}>
+      <TableContainer component={Paper}>
+      {/* <Table className={classes.table} aria-label="customized table" > */}
+      <Table style={{width:"750px"}} aria-label="customized table" >
+
         <TableHead>
           <TableRow>
             <StyledTableCell></StyledTableCell>
@@ -77,5 +89,8 @@ export default function CustomizedTables() {
         </TableBody>
       </Table>
     </TableContainer>
+
+    </div>
+    
   );
 }
